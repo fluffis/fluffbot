4b11b194ffc7094f98838084a293b312282b75e9
Fluff <fluff.svwp@gmail.com>
2017-05-07 15:08:40 +0000

    New list for autopatrolled

--- Lastactivelist.pl
+++ Lastactivelist.pl
@@ -71,3 +71,23 @@ foreach($bot->get_allusers("500", "rollbacker")) {
 $page .= qq!\n|}!;
 $bot->edit("User:Fluffbot/LAL-Tillbakarullare", $page, "Uppdaterar listan");
 
+# Autopatrolled
+
+$page = qq!Detta &auml;r en lista &ouml;ver konton p&aring; svwp som har [[WP:Projekt patrullering av nya sidor|autopatrullstatus]] och n&auml;r de senast var aktiva p&aring; svwp.!;
+$page .= qq! Listan uppdaterades senast: '''$today'''.!;
+$page .= qq!\n\n!;
+$page .= qq!{|class="wikitable sortable"\n!;
+$page .= qq<! Anv&auml;ndare !! Senaste redigering !! Antal redigeringar\n>;
+
+foreach($bot->get_allusers("500", "autopatrolled")) {
+    my $laldate = $bot->last_active($_);
+    $laldate =~ s/T.*//;
+#    $laldate =~ s/Z/\ UTC/;
+    my $count = $bot->count_contributions($_);
+    $page .= qq!|-\n| [[User:$_|$_]] ([[Special:Bidrag/$_|bidrag]]) || $laldate || $count\n!;
+    print "Processing user $_ - $laldate\n";
+    sleep 1;
+}
+
+$page .= qq!\n|}!;
+$bot->edit("User:Fluffbot/LAL-Autopatrullerade", $page, "Uppdaterar listan");
